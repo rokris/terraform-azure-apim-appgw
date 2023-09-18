a040ff7a935892f05bc784f99ae5c59ffc2afbc8
rokris <rokris@hotmail.com>
2023-09-18 21:52:59 +0200

    Syntax cleanup

--- apim/main.tf
+++ apim/main.tf
@@ -6,9 +6,27 @@ resource "azurerm_subnet" "apim-subnet" {
   address_prefixes          = [var.apim_subnet_iprange]
 }
 
-data "azurerm_network_security_group" "apim-nsg" {
+resource "azurerm_network_security_group" "apim-nsg" {
   name                = var.apim_nsg_name
+  location            = var.location
   resource_group_name = var.apim_rg
+  tags                = var.tags
+}
+
+# Create NSG rules
+resource "azurerm_network_security_rule" "rules" {
+  for_each                    = var.nsg_rules
+  name                        = each.key
+  priority                    = each.value.priority
+  direction                   = each.value.direction
+  access                      = each.value.access
+  protocol                    = each.value.protocol
+  source_address_prefix       = each.value.source_address_prefix
+  source_port_range           = each.value.source_port_range
+  destination_address_prefix  = each.value.destination_address_prefix
+  destination_port_range      = each.value.destination_port_range
+  resource_group_name         = var.apim_rg
+  network_security_group_name = azurerm_network_security_group.apim-nsg.name
 }
 
 resource "azurerm_subnet_network_security_group_association" "ass" {
