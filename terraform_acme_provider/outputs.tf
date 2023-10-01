b5c5d59fbb05900d2d420113b5b9e831cf2a80cb
rokris <rokris@hotmail.com>
2023-10-01 22:07:01 +0200

    Added privat_key output

--- terraform_acme_provider/outputs.tf
+++ terraform_acme_provider/outputs.tf
@@ -5,3 +5,7 @@ output "certificate_thumbprint" {
 output "pem" {
   value = data.azurerm_key_vault_certificate_data.example.pem
 }
+
+output "private_key_pem" {
+  value = nonsensitive(lookup(acme_certificate.certificate, "private_key_pem"))
+}
