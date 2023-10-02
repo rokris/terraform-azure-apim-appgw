18d0b783995773715cf7c8859a32c9f5b7762a58
rokris <rokris@hotmail.com>
2023-10-02 08:15:21 +0200

    Code fixup

--- apim/main.tf
+++ apim/main.tf
@@ -75,7 +75,7 @@ resource "azurerm_api_management" "apim" {
                     <inbound>
                       <cors allow-credentials="true">
                         <allowed-origins>
-                          <origin>${var.developer_portal_dns_name}</origin>
+                          <origin>https://${var.developer_portal_dns_name}</origin>
                         </allowed-origins>
                         <allowed-methods preflight-result-max-age="300">
                           <method>*</method>
