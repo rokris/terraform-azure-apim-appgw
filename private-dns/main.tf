3eee2319f4f10813003d4b82d707848caa71ade0
rokris <rokris@hotmail.com>
2023-10-02 11:56:48 +0200

    Changed to use CNAME in Private_Dns

--- private-dns/main.tf
+++ private-dns/main.tf
@@ -24,13 +24,24 @@ resource "azurerm_private_dns_zone_virtual_network_link" "virtual-link" {
   virtual_network_id    = data.azurerm_virtual_network.vnet.id
 }
 
+# #Create DNS Records
+# resource "azurerm_private_dns_a_record" "zone" {
+#   for_each            = { for record in local.dns_records : record.name => record }
+#   name                = each.value.name
+#   resource_group_name = each.value.resource_group_name
+#   zone_name           = each.value.zone_name
+#   ttl                 = each.value.ttl
+#   records             = [data.azurerm_api_management.private-ip.private_ip_addresses[0]]
+#   depends_on          = [azurerm_private_dns_zone.dns]
+# }
+
 #Create DNS Records
-resource "azurerm_private_dns_a_record" "zone" {
+resource "azurerm_private_dns_cname_record" "zone" {
   for_each            = { for record in local.dns_records : record.name => record }
   name                = each.value.name
   resource_group_name = each.value.resource_group_name
   zone_name           = each.value.zone_name
   ttl                 = each.value.ttl
-  records             = [data.azurerm_api_management.private-ip.private_ip_addresses[0]]
+  record              = "${var.apim_name}.azure-api.net"
   depends_on          = [azurerm_private_dns_zone.dns]
 }
