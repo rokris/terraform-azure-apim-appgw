94a0bdfdae84ac488ce8dcb73f8c6e9d1c93c491
Roger Kristiansen <ROGER.KRISTIANSEN@NORGESGRUPPEN.NO>
2024-01-21 20:49:49 +0100

    Moved catalgs around

--- waf_policy/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "ng-ti-test-rokris-rg"
-    storage_account_name = "ngtitestrokristfstate"
-    container_name       = "tfstate"
-    key                  = "waf_policy.terraform.tfstate"
-  }
-}
