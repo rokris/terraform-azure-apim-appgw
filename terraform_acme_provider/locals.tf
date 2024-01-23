8ca5662a8e61d8aaaee46e184323070e00f4fec1
Roger Kristiansen <rokris@hotmail.com>
2024-01-23 17:05:36 +0100

    Update locals.tf

--- terraform_acme_provider/locals.tf
+++ terraform_acme_provider/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  letsencrypt-staging = "letsencrypt-stg-root-x1"
-  letsencrypt-production = "letsencrypt-stg-root-x1"
+  letsencrypt-staging = "https://acme-staging-v02.api.letsencrypt.org/directory"
+  letsencrypt-production = "https://acme-v02.api.letsencrypt.org/directory"
 }
