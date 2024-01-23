2926de8ede6123315fd36f16bf4d0f4d466840c9
Roger Kristiansen <rokris@hotmail.com>
2024-01-23 16:17:59 +0100

    Update main-staging.tf

--- appgw/main-staging.tf
+++ appgw/main-staging.tf
@@ -257,7 +257,8 @@ resource "azurerm_application_gateway" "main" {
     protocol                       = "Https"
     request_timeout                = 60
     probe_name                     = var.probe_name
-    trusted_root_certificate_names = ["letsencrypt-stg-root-x1", ]
+    #trusted_root_certificate_names = ["letsencrypt-stg-root-x1", ]
+    var.environment != "production" ? trusted_root_certificate_names = ["letsencrypt-stg-root-x1", ] : ""
   }
   
   trusted_root_certificate {
